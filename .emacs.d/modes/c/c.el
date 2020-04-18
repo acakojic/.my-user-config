575627fd5f703beb16df0be0d24bcdd81120206c
Aca Kojic <aleksandar.kojic@logisoft.rs>
2020-04-18 13:06:30 +0200

    test

--- .emacs.d/modes/c/c.el
+++
@@ -1,8 +0,0 @@
-(setq-default c-basic-offset 4)
-
-(setq c-default-style "linux"
-          c-basic-offset 4)
-
-(setq-default c-basic-offset 4
-                  tab-width 4
-                  indent-tabs-mode t)
